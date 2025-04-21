ebcc7c5f864a197ab10d06ab3c3edf58e7f102bd
jcssll <essentialnetworker@gmail.com>
2025-04-21 14:49:08 -0400

    Update app.routes.ts
    Added route for top stories component

--- hacker-news-client/src/app/app.routes.ts
+++ hacker-news-client/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { TopStoriesComponent } from './components/top-stories/top-stories.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: 'top-stories', component: TopStoriesComponent}
+
+];
