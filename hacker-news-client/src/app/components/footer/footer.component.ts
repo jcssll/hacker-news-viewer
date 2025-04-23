fa44416489ee9b3ec31b7ba1978dcfa6698b574b
jcssll <essentialnetworker@gmail.com>
2025-04-22 22:47:52 -0400

    Set up footer component
    Footer Component created as a standalone - fully styled with links to Angular Docs and personal website

---
+++ hacker-news-client/src/app/components/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  standalone: true, 
+  selector: 'app-footer',
+  imports: [CommonModule],
+  templateUrl: './footer.component.html',
+  styleUrl: './footer.component.css'
+})
+export class FooterComponent {
+
+  
+
+}
