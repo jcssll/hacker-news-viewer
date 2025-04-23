098f92cd3d837f1501d960a69d03ed0dc0c66424
jcssll <essentialnetworker@gmail.com>
2025-04-23 13:15:56 -0400

    Update story-list.component.ts
    Imported OnInit to use loadStories method to use the service to get new stories.

--- hacker-news-client/src/app/components/story-list/story-list.component.ts
+++ hacker-news-client/src/app/components/story-list/story-list.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'
+import { HackerNewsService } from '../../services/hacker-news.service';
 
 @Component({
   selector: 'app-story-list',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './story-list.component.html',
   styleUrl: './story-list.component.css'
 })
-export class StoryListComponent {
+export class StoryListComponent implements OnInit{
+
+  storyType: 'top' | 'new'  | 'best' = 'top'; 
+  searchTerm: string = ''; 
+  page: number = 1; 
+  stories: any[]= [];
+
+  constructor(private hnService: HackerNewsService){}
+
+  ngOnInit(): void {
+    this.loadStories(); 
+  }
+
+  loadStories(): void {
+    this.hnService.getStories(this.storyType, this.page, 10, this.searchTerm). subscribe(data=> this.stories = data); 
+  }
 
 }
