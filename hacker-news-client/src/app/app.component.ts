3abbb6d4fe75e110c1a3018c149c119ac6e1be65
jcssll <essentialnetworker@gmail.com>
2025-04-22 22:43:49 -0400

    Update app.component.ts
    For now importing routeroutlet as priority- to render data from api

--- hacker-news-client/src/app/app.component.ts
+++ hacker-news-client/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+// import { HeaderComponent } from './components/header/header.component';
+// import { FooterComponent } from './components/footer/footer.component';
+// import { TopStoriesComponent } from './components/top-stories/top-stories.component';
 
 @Component({
+  standalone:true,
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet],
+ //imports: [ HeaderComponent, FooterComponent, TopStoriesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
+  // template: `//I should be defining this in the app.component.html
+  //   <app-header></app-header>
+  //   <app-top-stories></app-top-stories>
+  //   <app-footer></app-footer>
+  // `, 
+   //imports: [HeaderComponent, FooterComponent, TopStoriesComponent],
 })
 export class AppComponent {
-  title = 'hacker-news-client';
+  //title = 'hacker-news-client';
 }
