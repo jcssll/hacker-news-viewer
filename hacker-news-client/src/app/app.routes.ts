a15fe54523842205dba59263ceee18fcf022e8a2
jcssll <essentialnetworker@gmail.com>
2025-04-23 13:04:47 -0400

    Update app.routes.ts
    This Angular router configuration defines two routes: a root route (path: '') that loads the StoryListComponent, and a wildcard route (path: '**') that redirects to the root route
    In Detail:
    path: '': This route matches the empty URL path (/) of the application.
    loadComponent: This property is used to dynamically load the StoryListComponent when the route is matched. The import() function returns a promise that resolves to a module, and then(m => m.StoryListComponent) extracts the component from the module.
    path: '**': This wildcard route matches any URL path that doesn't match any other defined routes.
    redirectTo: '': This property specifies that any unmatched route should be redirected to the root route (/).

--- hacker-news-client/src/app/app.routes.ts
+++ hacker-news-client/src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
-import { TopStoriesComponent } from './components/top-stories/top-stories.component';
+// import { TopStoriesComponent } from './components/top-stories/top-stories.component';
+import { StoryListComponent } from './components/story-list/story-list.component';
 import { AppComponent } from './app.component';
 
 export const routes: Routes = [
 
-    {path: '', component: TopStoriesComponent}, 
-    {path: 'Home', component: AppComponent}
-    
-
-
+    {
+        path: '',
+        loadComponent: () => import('./components/story-list/story-list.component').then(m => m.StoryListComponent) 
+     }, 
+    {path: '**', redirectTo: ''}
 ];
