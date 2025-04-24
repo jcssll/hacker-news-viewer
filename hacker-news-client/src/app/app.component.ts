557467ff5f2173d28986f0a11056ea29605071e3
jcssll <essentialnetworker@gmail.com>
2025-04-23 23:08:19 -0400

    App Component
    imported Header and Footer components with the correct path

--- hacker-news-client/src/app/app.component.ts
+++ hacker-news-client/src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-// import { HeaderComponent } from './components/header/header.component';
-// import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 // import { TopStoriesComponent } from './components/top-stories/top-stories.component';
 
 @Component({
   standalone:true,
   selector: 'app-root',
-  imports: [ RouterOutlet],
- //imports: [ HeaderComponent, FooterComponent, TopStoriesComponent],
+ // imports: [ RouterOutlet],
+ imports: [ HeaderComponent, FooterComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
   // template: `//I should be defining this in the app.component.html
