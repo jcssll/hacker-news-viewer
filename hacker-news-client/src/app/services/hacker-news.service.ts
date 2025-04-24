baab41b17c2687b788bf47105257a57228494fb9
jcssll <essentialnetworker@gmail.com>
2025-04-24 14:39:25 -0400

    Update hacker-news.service.ts
    Updated path to go to either the development or production environment

--- hacker-news-client/src/app/services/hacker-news.service.ts
+++ hacker-news-client/src/app/services/hacker-news.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,8 @@ import { Observable } from 'rxjs';
 export class HackerNewsService {
 
   //private apiUrl = 'https://localhost:5001/api/stories/top';
-  private baseUrl = 'https://localhost:5001/api/stories';
+ // private baseUrl = 'https://localhost:5001/api/stories';
+ private baseUrl = environment.apiUrl; 
 
   constructor(private http: HttpClient) { }
 
